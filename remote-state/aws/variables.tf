762ae21620a7f203035c6e31ced3aad2a96fc83a
John Morsley <johnmorsley@gmail.com>
2019-12-30 09:09:34 +0000

    Update variables.tf

--- remote-state/aws/variables.tf
+++ remote-state/aws/variables.tf
@@ -1,9 +1,5 @@
-variable "aws_access_key" {
-  default = "AKIA4JXLP2XJ4IUOVWE5"
-}
-variable "aws_secret_key" {
-  default = "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"
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 variable "aws_terraform_bucket" {
   default = "morsley-uk-terraform"
